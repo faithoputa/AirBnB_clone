56dd15414ac64e3ea8be6289e4e0d69eb08b7eda
HavallonTech <kestercode@gmail.com>
2024-01-10 02:38:37 +0100

    AirBnB

--- models/base_model.py
+++ models/base_model.py
@@ -12,26 +12,32 @@ class BaseModel:
     """ this is the BaseModel class """
 
     def __init__(self, *args, **kwargs):
-        """Public instance attributes: common to the entire classes"""
+        """Public instance attributes: common to the entire classes
+        ARGS: Args, and Kwargs cautionary parameters
+        """
+
         self.id = str(uuid.uuid4())
-        dt = datetime(2017, 6, 14, 22, 31, 3, 285259)
+        dt = datetime.today()
         self.created_at = dt.isoformat()
-        up_at = datetime(2017, 6, 14, 22, 31, 3, 285259)
+        up_at = datetime.today()
         self.updated_at = up_at.isoformat()
-        self.data_collection = {
-            "id": self.id,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at
-        }
 
     def __str__(self):
         """The __str__ method to print classname, id, dictionary data """
         return (f"[<class name>] (<self.id>) <self.__dict__>:to be printed here")
 
     def save(self):
-        pass
+        self.updated_at = datetime.now().isoformat()
+
+    def to_dict(self):
+        self.data_collection = {
+            "id": self.id,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
+        return (self.data_collection)
 
 
 if __name__ == "__main__":
     newobj = BaseModel()
-    print(newobj.data_collection)
+    # print(newobj.to_dict())
