e9382e46bea0d468761b5e7b4a5e8febed26b83e
HavallonTech <kestercode@gmail.com>
2024-01-17 00:15:50 +0100

    Air BnB

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,29 +1,52 @@
 #!/usr/bin/python3
-"""Test BaseModel for expected behavior and documentation"""
-import inspect
-import json
-import models
-import pep8 as pycodestyle
-import time
+""" Module for Test cases for the base model """
+import datetime
+import io
+import sys
 import unittest
-from unittest.case import SkipTest
-from unittest.mock import patch
 from models.base_model import BaseModel
 
 
-class TestBaseModelDocs(unittest.TestCase):
-    """Tests to check the documentation of BaseModel class"""
+class Test_Base_model_class(unittest.TestCase):
+    """ Class for Test Base model """
 
-    @classmethod
-    def setUpClass(cls):
-        """Set up for docstring tests"""
-        cls.base_funcs = inspect.getmembers(BaseModel, inspect.isfunction)
+    def test_func(self):
+        """ function for testing str, save, to_dict"""
+        test_model = BaseModel()
 
-    def test_pep8_conformance(self):
-        """Test that models/base_model.py conforms to PEP8."""
-        for path in ['models/base_model.py',
-                     'tests/test_models/test_base_model.py']:
-            with self.subTest(path=path):
-                errors = pycodestyle.Checker(path).check_all()
-                self.assertEqual(errors,   
+        test_model.name = "first_test_model"
+        test_model.my_number = 97
 
+        s = str(test_model)
+        self.assertIn("[BaseModel]", s)
+        self.assertIn(test_model.id, s)
+        self.assertIn(str(test_model.__dict__), s)
+
+        test_model.save()
+
+        s = str(test_model)
+        self.assertIn("[BaseModel]", s)
+        self.assertIn(test_model.id, s)
+        self.assertIn(str(test_model.__dict__), s)
+
+        d = test_model.to_dict()
+
+        self.assertIsInstance(d, dict)
+
+        self.assertIn("id", d)
+        self.assertIn("created_at", d)
+        self.assertIn("updated_at", d)
+        self.assertIn("name", d)
+        self.assertIn("my_number", d)
+        self.assertIn("__class__", d)
+
+    def test_04_baseModel(self):
+        """ test attributes of basemodel after creation """
+
+        test_model = BaseModel()
+        test_model_json = test_model.to_dict()
+        test_new_model = BaseModel(**test_model_json)
+
+        self.assertIsNotNone(test_new_model.id)
+        self.assertIsInstance(test_new_model.created_at, datetime.datetime)
+        self.assertIsInstance(test_new_model.updated_at, datetime.datetime)
