d7230a8c620589e56d1af20cc08c5695b48a20c4
HavallonTech <kestercode@gmail.com>
2024-01-17 00:24:55 +0100

    Air BnB

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""" module for testing the city class"""
+from models.city import City
+import unittest
+
+
+class test_city(unittest.TestCase):
+    """ class to test for the city """
+
+    def test_city_class(self):
+        """ test the city """
+        city_class_obj = City()
+
+        self.assertEqual(city_class_obj.name, "")
+        self.assertEqual(city_class_obj.state_id, "")
+
+        string_form = str(city_class_obj)
+        self.assertIn("[City]", string_form)
+        self.assertIn(city_class_obj.id, string_form)
+        self.assertIn(str(city_class_obj.__dict__), string_form)
